bde17199a66e6a0ddf47c85566fb2a9c6b464b1c
danilo0792 <danilo_sp06@hotmail.com>
2014-02-11 10:23:17 -0200

    project angularjs

---
+++ Express-com-AngularJS/app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('ExpressComAngularJSApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
